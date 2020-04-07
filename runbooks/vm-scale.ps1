8307a268b93ce2351e182440107018e24ce4708d
l3oferreira <31907205+l3oferreira@users.noreply.github.com>
2020-04-07 19:20:06 -0300

    Consuming subscription id from connection.

--- runbooks/vm-scale.ps1
+++ runbooks/vm-scale.ps1
@@ -10,53 +10,54 @@ Note: Downtime of +- 4 minutes applies when the job starts.
    This script will adjust the size of a Microsoft Azure virtual machine based on input given into the new schedule.
 
    When making a Schedule or doing a test run you are required to declare:
-   * The subscription of the VM that is to be changed
-   * The resource group name of the VM that is to be changed
-   * The name of the VM that is to be changed
-   * The desired to be VM Size
+   * Connection to manipulate Azure resources
+   * Resource group name of the VM that is to be changed
+   * Name of the VM that is to be changed
+   * Desired to be VM Size
    
-   This script is based on the runbook by Siebert Timmermans, 90% of credit goes to him.
+   This script is based on the runbook by Siebert Timmermans, 80% of credit goes to him.
    https://siebertt.github.io/Azure-ResourcegroupVM-Rescaling-Scheduler/
 
-.PARAMETER SubscriptionId
-    String that contains the id of subscription where your specified VM is a part of.
+.PARAMETER ConnectionName
+    String that contains the name of connection to manipulate Azure resources.
 
-.PARAMETER ResourceGroupName
+.PARAMETER ResourceGroup
     String that contains the name of resource group where your specified VM is a part of.
-    
+
+.PARAMETER VMSize
+     String name of the desired size the specified VM will be receiving.
+
 .PARAMETER VirtualMachineName
      String name of the VM that will be changed in size.
 
-.PARAMETER VirtualMachineSize
-     String name of the desired size the specified VM will be receiving.
-
 .NOTES
      Author: Leonardo Ferreira
-     Last Updated: 04/02/2020
-     Version 1.0 - Changed Siebert Timmermans`s script to change connection.
+     Last Updated: 04/07/2020
+     
+     Version 1.0.1 - Consuming subscription id from connection.
+     Version 1.0.0 - Changed Siebert Timmermans`s script to change connection.
 #>
-workflow VMScale {
+Workflow VMScheduledScaler {
 	
-    Param (    
-        [parameter(Mandatory=$true)]
-        [string] $SubscriptionId,
+    Param (
+        [Parameter(Mandatory=$true)] 
+        [String] $ConnectionName="AzureRunAsConnection",
 
-        [parameter(Mandatory=$true)]
-        [string] $ResourceGroupName,    
+        [parameter(Mandatory=$true)][ValidateNotNullOrEmpty()]
+        [string] $ResourceGroup,    
 
-        [parameter(Mandatory=$true)]
+        [parameter(Mandatory=$true)][ValidateNotNullOrEmpty()]
         [String] $VirtualMachineName,
         
-        [parameter(Mandatory=$true)]
-        [string] $VirtualMachineSize
-   
+        [parameter(Mandatory=$true)][ValidateNotNullOrEmpty()]
+        [string] $VMSize
     )
-    InlineScript {
+	InlineScript {
 
         $connection = Get-AutomationConnection `
-            -Name 'AzureRunAsConnection'
+            -Name $Using:ConnectionName
 
-        Write-Output "`nConnected using AzureRunAsConnection"
+        Write-Output "`nConnected using $Using:ConnectionName"
 
         Add-AzureRmAccount -ServicePrincipal `
             -TenantId $connection.TenantId `
@@ -64,14 +65,14 @@ workflow VMScale {
             -CertificateThumbprint $connection.CertificateThumbprint
 
         Select-AzureRmSubscription `
-            -SubscriptionId $Using:SubscriptionId
+            -SubscriptionId $connection.SubscriptionId
 
-        Write-Output "`nSet current subscription: $Using:SubscriptionId"
+        Write-Output "`nSet current subscription"
 
         # Check if specified VM can be found
         Try {
             $vm = Get-AzureRmVm `
-                -ResourceGroupName $Using:ResourceGroupName `
+                -ResourceGroupName $Using:ResourceGroup `
                 -VMName $Using:VirtualMachineName `
                 -ErrorAction Stop
 
@@ -88,18 +89,18 @@ workflow VMScale {
         Write-Output "Current size: $currentVMSize"
 
         # Change to new VM Size and report
-	$newVMSize = $Using:VirtualMachineSize
+	    $newVMSize = $Using:VMSize
     
         Write-Output "`nNew size will be: $newVMSize"
         Write-Output "`n----------------------------------------------------------------------"
             
         $vm.HardwareProfile.VmSize = $newVMSize
-        Update-AzureRmVm -VM $vm -ResourceGroupName $Using:ResourceGroupName
+        Update-AzureRmVm -VM $vm -ResourceGroupName $Using:ResourceGroup
         
-        $updatedVm = Get-AzureRmVm -ResourceGroupName $Using:ResourceGroupName -VMName $Using:VirtualMachineName
+        $updatedVm = Get-AzureRmVm -ResourceGroupName $Using:ResourceGroup -VMName $Using:VirtualMachineName
         $updatedVMSize = $updatedVm.HardwareProfile.vmSize
         
         Write-Output "`n----------------------------------------------------------------------"
         Write-Output "`nSize updated to: $updatedVMSize"
-    }
+ 	}
 }
